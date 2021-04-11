2dc46630fcaedf8073baa20ad1c7d0c5f8f29719
DragonRollGH <492782447@qq.com>
2021-04-11 13:14:59 +0800

    class Cursor

--- wechat/pages/pulser/Cursor.js
+++ wechat/pages/pulser/Cursor.js
@@ -1,9 +1,12 @@
 const DPR = getApp().globalData.dpr;
 
+import PixelPos from "PixelPos"
+
 class Cursor {
-  constructor() {
+  constructor(touchBox) {
     this.x = undefined;
     this.y = undefined;
+    this.touchBox = touchBox;
     this.identifier = undefined;
     this.pixelIds = [undefined, undefined]; //index 0 is the old id, 1 is the new id
   }
@@ -29,13 +32,16 @@ class Cursor {
       this.pixelIds[0] = undefined;
       return
     }
-
+    let dist = 100;
     for (let i in PixelLen) {
-      dis = Math.sqrt((this.x - PixelPos[i][0]) ** 2 + (this.y - PixelPos[i][1]) ** 2);
-      if (dis <= TouchBoxR) {
+      ndist = Math.sqrt((this.x - PixelPos[i][0]) ** 2 + (this.y - PixelPos[i][1]) ** 2);
+      if (ndist < dist) {
+        dist = ndist;
         this.pixelIds[1] = i;
-        break;
       }
+    }
+    if (dist > this.touchBox) {
+      this.pixelIds[1] = undefined;
     }
   }
 }
